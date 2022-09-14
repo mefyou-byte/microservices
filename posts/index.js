d08369d5014989d59803ac3f1fabb229aa727d5c
Faltas <mfaltas@dccsintra.net>
2022-09-14 16:53:38 +0200

    post service

---
+++ posts/index.js
@@ -0,0 +1,30 @@
+const express = require('express'); 
+const app = express(); 
+const {randomBytes} = require('crypto'); 
+const bodyparser = require('body-parser'); 
+
+
+app.use(bodyparser.json()); 
+
+
+const posts = {}
+app.get('/posts', (req, res) => {
+    res.send(posts);   
+}); 
+
+
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex'); 
+    const {title} = req.body; 
+
+    posts[id] = {
+        id, title
+    }
+
+    res.status(201).send(posts[id]); 
+}); 
+
+app.listen(4000, () => {
+    console.log('Listeneing on Port 4000'); 
+})
